8ec8a9317cc8b8d0327c3ff06f81837b3b4188e4
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2023-02-25 16:30:26 +0200

    Create FeedbackOptions.styled.jsx

---
+++ src/components/FeedbackOptions/FeedbackOptions.styled.jsx
@@ -0,0 +1,25 @@
+import styled from '@emotion/styled';
+
+export const ButtonList = styled.ul`
+padding: 10px 10px;
+background-color: white;
+border-radius: 8px;
+display: flex;
+justify-content: flex-start;
+align-items: center;
+`;
+
+export const ButtonItem = styled.button`
+display: inline-block;
+padding: 15px 15px;
+margin: 10px;
+font-size: 12px;
+cursor: pointer;
+text-align: center;
+text-decoration: none;
+outline: none;
+color: #fff;
+background-color: #4CAF50;
+border: none;
+border-radius: 15px;
+`;
